2bca5dfc684577469843237918c8bc42af4cc46b
Joe <josehalerner@gmail.com>
2017-09-23 14:01:21 -0700

    Complete hw01 assignment

--- hw01/src/HW01.hs
+++ hw01/src/HW01.hs
@@ -1,16 +1,38 @@
 module HW01 where
+import Data.Char
 
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits num = if (num <= 0) then []
+  else (map (\c -> (charToInteger c)) (integerToString num))
+
+integerToString :: Integer -> String
+integerToString num = (show num)
+
+charToInteger :: Char -> Integer
+charToInteger c = toInteger (digitToInt c)
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev num = reverse (toDigits num)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+-- reverse the list before doubling so that every second digit from the right is doubled.
+-- otherwise, every second digit from the left would be doubled
+doubleEveryOther nums = reverse (doubleEveryOtherAux (reverse (nums)))
+
+doubleEveryOtherAux :: [Integer] -> [Integer]
+doubleEveryOtherAux [] = []
+doubleEveryOtherAux (head:[]) = [head]
+doubleEveryOtherAux (head:next:tail) = head : next * 2 : doubleEveryOtherAux(tail)
 
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits nums = foldl (+) 0 (toDigits(concatIntegerLists (nums)))
+
+-- took this function from:
+-- https://stackoverflow.com/questions/1918486/convert-list-of-integers-into-one-int-like-concat-in-haskell
+-- is it generally OK to take helper functions like these and cite them? I'm supposing yes?
+concatIntegerLists :: [Integer] -> Integer
+concatIntegerLists = foldl addDigit 0
+  where addDigit num d = 10 * num + d
 
 validate :: Integer -> Bool
-validate = undefined
+validate num = (sumDigits(doubleEveryOther (toDigits (num))) `mod` 10) == 0
